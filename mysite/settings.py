f0ec1f96a982e43a1c005383a403e1f0af98ff94
Pooja Patel <poojapatel@nyu.edu>
2022-10-19 03:39:09 -0400

    added travis

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-vu973npu.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-vu973npu.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
